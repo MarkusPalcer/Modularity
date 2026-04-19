4ce6128e50c5c250c4793fac3dce88bf6a8dd75c
Markus Palcer <MasterOfVDL@GMail.com>
2026-04-19 21:51:38 +0200

    improve

--- MarkusPalcer.Modularity/IFactory.cs
+++ MarkusPalcer.Modularity/IFactory.cs
@@ -2,10 +2,16 @@
 
 namespace MarkusPalcer.Modularity;
 
-public interface IFactory {}
+public interface IFactory;
 
 [UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
 public interface IFactory<in TIn, out T> : IFactory
 {
     T Create(TIn param);
+}
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
+public interface IFactory<in TIn1, in TIn2, out T> : IFactory
+{
+    T Create(TIn1 param1, TIn2 param2);
 }
